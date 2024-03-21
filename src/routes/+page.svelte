c0ec0f4110f1c6c87cd3a9e9591eca3fd8f93434
Paarth314 <paarthiyer1234@gmail.com>
2024-03-21 19:17:52 +0530

    formatted files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button
+    type="button"
+    class="m-4 rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-75 hover:scale-75 hover:bg-blue-700"
+    >TEST</button
+>
