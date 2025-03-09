e4afc7c478b50b993d65a84926f0bd5e2744ca7e
PaarthIyer <paarthiyer1234@gmail.com>
2025-03-10 00:25:22 +0530

    hopefully fixed rendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-static'
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
@@ -10,16 +10,7 @@ const config = {
 
     kit: {
         adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            },
-            platformProxy: {
-                configPath: undefined,
-                environment: undefined,
-                persist: undefined
-            }
+            out: 'build'
         })
     }
 }
