15d422237ea17dd386c8f0e40d09052c8029f5fc
PaarthIyer <paarthiyer1234@gmail.com>
2025-03-10 00:00:09 +0530

    keys

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+
+
+const PUBLIC_SUPABASE_URL='https://jjsxgilfjuombykmbdxj.supabase.co'
+const PUBLIC_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impqc3hnaWxmanVvbWJ5a21iZHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NTQ0MDQsImV4cCI6MjA1NTIzMDQwNH0.nvUn7KszyMNir5FA0MpGQIZsvncf3eq1dzVfEbeg38M'
 
 const supabaseUrl = PUBLIC_SUPABASE_URL
 const supabaseKey = PUBLIC_SUPABASE_ANON_KEY
