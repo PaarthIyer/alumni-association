2cf6b1ac4d104a4c98e4fb7b20de22246ae42083
PaarthIyer <paarthiyer1234@gmail.com>
2025-03-17 01:05:47 +0530

    added footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    import Footer from '$lib/components/Footer.svelte'
     import '../app.css'
     let { children } = $props()
 </script>
 
 <div style="color:var(--text-body-color)">{@render children()}</div>
+<Footer></Footer>
