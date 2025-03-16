bdd16e90469a405825b6e42730322796ece3e227
PaarthIyer <paarthiyer1234@gmail.com>
2025-03-17 02:32:34 +0530

    alum dir placeholder

---
+++ src/routes/alumni/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from '$lib/components/Navbar.svelte'
+</script>
+
+<Navbar transparent={false}></Navbar>
+<div class="flex h-screen">
+    <div class="font-ubuntu m-auto text-center text-4xl font-medium">
+        The Alumni Directory is under construction.<br />Thank you for your patience
+    </div>
+</div>
